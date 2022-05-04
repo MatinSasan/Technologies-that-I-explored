aa6107c4731eaa2f04faa3f06828c5864009462f
Matin Sasanpour <matin.sasan@gmail.com>
2022-05-04 18:06:45 +0430

    added todo-app done in 3 py-backend-frameworks

---
+++ Python/python_backend_frameworks/To_do_app_in_3_frameworks/fastapi/models.py
@@ -0,0 +1,12 @@
+from email.policy import default
+from sqlalchemy import Boolean, Column, Integer, String
+
+from database import Base
+
+
+class Todo(Base):
+    __tablename__ = 'todos'
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    complete = Column(Boolean, default=False)
